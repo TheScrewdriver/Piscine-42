98ed3b6d3d070c0edc600cb82f7b774bf8268f2f
Romann BROQUE <rbroque@e3r6p5.clusters.42paris.fr>
2022-07-22 22:48:08 +0200

    auhsduoa

---
+++ ex00/includes/rush02.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 22:00:02 by rbroque           #+#    #+#             */
+/*   Updated: 2022/07/22 22:07:54 by rbroque          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RUSH02_H
+# define RUSH02_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <errno.h>
+
+#endif
